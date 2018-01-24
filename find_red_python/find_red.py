f85e7f2b03b984c06fa25deda66f854a6db81bc9
Jamborinski <jamborinski@gmail.com>
2018-01-24 10:33:21 -0800

    Add files via upload

---
+++ find_red_python/find_red.py
@@ -0,0 +1,34 @@
+# import the necessary packages
+import sys
+import numpy as np
+import cv2
+
+def main(argv):
+   if len(sys.argv) is not 2 :
+      sys.exit("Usage: python find_red.py <image>.jpg")
+   # load the image
+   image = cv2.imread(argv[1])
+    
+   # find the red color in the image
+   upper = np.array([65, 65, 255])
+   lower = np.array([0, 0, 200])
+   
+
+   mask = cv2.inRange(image, lower, upper)
+
+   # find contours in the masked image and keep the largest one
+   (_, cnts, _) = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, 
+                  cv2.CHAIN_APPROX_SIMPLE)
+   c = max(cnts, key=cv2.contourArea)
+    
+   # approximate the contour
+   peri = cv2.arcLength(c, True)
+   approx = cv2.approxPolyDP(c, 0.05 * peri, True)
+    
+   # draw an ID tag on/surrounding the red object
+   cv2.drawContours(image, [approx], -1, (0, 255, 0), 4)
+   cv2.imshow("Image", image)
+   cv2.waitKey(0)
+
+if __name__ == "__main__":
+   main(sys.argv)
